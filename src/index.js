77667052d40cb0172f3af61d73208bb25c029e6c
Rammoun <44053646+Rammoun@users.noreply.github.com>
2018-11-19 09:52:16 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ZeroNb=()=>{
+    return <span className='zero'>00</span>
+}
+const Thirty=()=>{
+    return <span className='thirty'>:30:</span>
+}
+const Txt=()=>{
+    return <h6 className='txt'>Hours Minutes Seconds</h6>
+}
+ReactDOM.render(
+    <div className='full'>
+     <div className='digits'>
+      <ZeroNb/><Thirty/><ZeroNb/>
+     </div>
+     <Txt/>
+    </div>, 
+    document.getElementById('root')
+)
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
